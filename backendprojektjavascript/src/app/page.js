2092cea690ca2b2e16bc8e9735e5c448c22f630d
Simon kallkvist <simon.kallkvist@gmail.com>
2024-09-04 15:08:35 +0200

    added and understood

--- backendprojektjavascript/src/app/page.js
+++ backendprojektjavascript/src/app/page.js
@@ -1,12 +1,16 @@
+import Navbar from '@/components/navBar/Navbar';
 import Image from 'next/image';
 import React from 'react';
 
 const Home = () => {
   return (
-    <div className='h-screen flex flex-col items-center justify-center'>
-      <h1>Welcome</h1>
-      <button>Logout</button>
-    </div>
+    <>
+      <Navbar />
+      <div className='h-screen flex flex-col items-center justify-center'>
+        <h1>Welcome</h1>
+        <button>Logout</button>
+      </div>
+    </>
   );
 };
 
---
+++ backendprojektjavascript/src/components/navBar/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+const { useAuth } = require('@/contexts/auth');
+import LocalStorageKit from '@/utils/localStorageKit';
+
+const Navbar = () => {
+  //TODO Find the user
+
+  return <div>navBar</div>;
+};
+
+export default Navbar;
